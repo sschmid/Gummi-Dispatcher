3620ce1bebbc7db60697a21527577624f16dd2ac
Simon Schmid <mail@sschmid.com>
2013-01-11 11:01:17 +0100

    Added icon.

--- Gummi-Dispatcher/GDAppDelegate.m
+++ Gummi-Dispatcher/GDAppDelegate.m
@@ -13,12 +13,26 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     [self.window makeKeyAndVisible];
+
+    [self addIcon];
 
     Example *example = [[Example alloc] init];
 
     return YES;
+}
+
+- (void)addIcon {
+    UIImage *image = [UIImage imageNamed:@"Gummi-Dispatcher-144.png"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    CGRect centerFrame = CGRectMake(self.window.center.x - imageView.frame.size.width / 2,
+            self.window.center.y - imageView.frame.size.height / 2,
+            imageView.frame.size.width,
+            imageView.frame.size.height);
+
+    imageView.frame = centerFrame;
+    [self.window addSubview:imageView];
 }
 
 @end
